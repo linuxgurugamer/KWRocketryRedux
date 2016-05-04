64d8cdb25b7fc989cf55a4118277dfa006daa4b4
Jonathan Bayer <linuxgurugamer@gmail.com>
2016-05-04 08:57:33 -0400

    Added Dummies documents

--- buildit.bat
+++ buildit.bat
@@ -26,17 +26,26 @@ type KWRocketry\GameData\KWRocketry\KWRocketryRedux.version
 set /p VERSION= "Enter version: "
 
 
+copy "KW Redux Install For Dummies.docx" KWRocketry
+copy "KW Redux Install For Dummies.pdf" KWRocketry
+
 set FILE="%RELEASEDIR%\KWRocketryRedux-%VERSION%.zip"
 IF EXIST %FILE% del /F %FILE%
 %ZIP% a -tzip %FILE% KWRocketry
+
+del "KWRocketry\KW Redux Install For Dummies.docx"
+del "KWRocketry\KW Redux Install For Dummies.pdf"
 
 rem Now create an install directory which also contains a default config
 mkdir aa
 mkdir aa\KWRocketry
 xcopy /E KWRocketry aa\KWRocketry
 
+
 rem Copy the GraduatedPowerResponse
 xcopy /E KWRocketry\Extras\GraduatedPowerResponseConfigs\GameData\KWRocketry aa\KWRocketry\GameData\KWRocketry
+copy "KW Redux Install For Dummies Manual Install.docx" aa\KWRocketry
+copy "KW Redux Install For Dummies Manual Install.pdf" aa\KWRocketry
 cd aa
 
 set FILE="%RELEASEDIR%\KWRocketryRedux-MANUAL-%VERSION%.zip"
