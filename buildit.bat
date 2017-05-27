985f911ad944b93566e46c5c05ed2f59026b5e0a
Jonathan Bayer <linuxgurugamer@gmail.com>
2017-05-27 13:07:38 -0400

    Replaced missing sound file for optional patch to reduce launch clamp sounds Wrote patch to reduce the launch clamp sounds, replacing old file which overwrote the stock squad file Updated for 1.3

--- buildit.bat
+++ buildit.bat
@@ -1,29 +1,32 @@
-rem @echo off
-set DEFHOMEDRIVE=d:
-set DEFHOMEDIR=%DEFHOMEDRIVE%%HOMEPATH%
-set HOMEDIR=
-set HOMEDRIVE=%CD:~0,2%
+ @echo off
 
 set RELEASEDIR=d:\Users\jbb\release
 set ZIP="c:\Program Files\7-zip\7z.exe"
-echo Default homedir: %DEFHOMEDIR%
-
-set /p HOMEDIR= "Enter Home directory, or <CR> for default: "
-
-if "%HOMEDIR%" == "" (
-set HOMEDIR=%DEFHOMEDIR%
-)
-echo %HOMEDIR%
-
-SET _test=%HOMEDIR:~1,1%
-if "%_test%" == ":" (
-set HOMEDRIVE=%HOMEDIR:~0,2%
-)
 
 
 
-type KWRocketry\GameData\KWRocketry\KWRocketryRedux.version
-set /p VERSION= "Enter version: "
+copy KWRocketry\GameData\KWRocketry\KWRocketryRedux.version a.version
+set VERSIONFILE=a.version
+rem set /p VERSION= "Enter version: "
+
+rem The following requires the JQ program, available here: https://stedolan.github.io/jq/download/
+c:\local\jq-win64  ".VERSION.MAJOR" %VERSIONFILE% >tmpfile
+set /P major=<tmpfile
+
+c:\local\jq-win64  ".VERSION.MINOR"  %VERSIONFILE% >tmpfile
+set /P minor=<tmpfile
+
+c:\local\jq-win64  ".VERSION.PATCH"  %VERSIONFILE% >tmpfile
+set /P patch=<tmpfile
+
+c:\local\jq-win64  ".VERSION.BUILD"  %VERSIONFILE% >tmpfile
+set /P build=<tmpfile
+del tmpfile
+set VERSION=%major%.%minor%.%patch%
+if "%build%" NEQ "0"  set VERSION=%VERSION%.%build%
+
+echo %VERSION%
+del a.version
 
 
 copy "KW Redux Install For Dummies.docx" KWRocketry
